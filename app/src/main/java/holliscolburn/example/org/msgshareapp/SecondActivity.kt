80103b73cad7e9c4eeb2dd91080c3bf26a3c42a1
holliscolburn <colburnh@oregonstate.edu>
2021-07-21 21:28:12 -0700

    section 2 video 5

--- app/src/main/java/holliscolburn/example/org/msgshareapp/SecondActivity.kt
+++ app/src/main/java/holliscolburn/example/org/msgshareapp/SecondActivity.kt
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val bundle : Bundle? = intent.extras
+        val msg = bundle!!.getString("user_message")
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+        txvUserMessage.text = msg
     }
 
 }
