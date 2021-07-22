172dd6c2c1eacab913172fb0a67ad8747b3b2f1b
holliscolburn <colburnh@oregonstate.edu>
2021-07-21 18:28:57 -0700

    section 2 video 3

---
+++ app/src/main/java/holliscolburn/example/org/msgshareapp/SecondActivity.kt
@@ -0,0 +1,16 @@
+package holliscolburn.example.org.msgshareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+}
